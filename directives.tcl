941f28b3b739865d3753159fe09a6d8c461ce6b5
Ales Svetek <ales.svetek@cern.ch>
2018-11-26 21:36:22 +0100

    Add required directives.tcl file

---
+++ directives.tcl
@@ -0,0 +1,114 @@
+set_directive_pipeline -II 1 "algo_top"
+set_directive_unroll "algo_top"
+
+set_directive_interface -mode axis  -register_mode  off -name link_in_00 "algo_top" link_in_00
+set_directive_interface -mode axis  -register_mode  off -name link_in_01 "algo_top" link_in_01
+set_directive_interface -mode axis  -register_mode  off -name link_in_02 "algo_top" link_in_02
+set_directive_interface -mode axis  -register_mode  off -name link_in_03 "algo_top" link_in_03
+set_directive_interface -mode axis  -register_mode  off -name link_in_04 "algo_top" link_in_04
+set_directive_interface -mode axis  -register_mode  off -name link_in_05 "algo_top" link_in_05
+set_directive_interface -mode axis  -register_mode  off -name link_in_06 "algo_top" link_in_06
+set_directive_interface -mode axis  -register_mode  off -name link_in_07 "algo_top" link_in_07
+set_directive_interface -mode axis  -register_mode  off -name link_in_08 "algo_top" link_in_08
+set_directive_interface -mode axis  -register_mode  off -name link_in_09 "algo_top" link_in_09
+
+set_directive_interface -mode axis  -register_mode  off -name link_in_10 "algo_top" link_in_10
+set_directive_interface -mode axis  -register_mode  off -name link_in_11 "algo_top" link_in_11
+set_directive_interface -mode axis  -register_mode  off -name link_in_12 "algo_top" link_in_12
+set_directive_interface -mode axis  -register_mode  off -name link_in_13 "algo_top" link_in_13
+set_directive_interface -mode axis  -register_mode  off -name link_in_14 "algo_top" link_in_14
+set_directive_interface -mode axis  -register_mode  off -name link_in_15 "algo_top" link_in_15
+set_directive_interface -mode axis  -register_mode  off -name link_in_16 "algo_top" link_in_16
+set_directive_interface -mode axis  -register_mode  off -name link_in_17 "algo_top" link_in_17
+set_directive_interface -mode axis  -register_mode  off -name link_in_18 "algo_top" link_in_18
+set_directive_interface -mode axis  -register_mode  off -name link_in_19 "algo_top" link_in_19
+
+set_directive_interface -mode axis  -register_mode  off -name link_in_20 "algo_top" link_in_20
+set_directive_interface -mode axis  -register_mode  off -name link_in_21 "algo_top" link_in_21
+set_directive_interface -mode axis  -register_mode  off -name link_in_22 "algo_top" link_in_22
+set_directive_interface -mode axis  -register_mode  off -name link_in_23 "algo_top" link_in_23
+set_directive_interface -mode axis  -register_mode  off -name link_in_24 "algo_top" link_in_24
+set_directive_interface -mode axis  -register_mode  off -name link_in_25 "algo_top" link_in_25
+set_directive_interface -mode axis  -register_mode  off -name link_in_26 "algo_top" link_in_26
+set_directive_interface -mode axis  -register_mode  off -name link_in_27 "algo_top" link_in_27
+set_directive_interface -mode axis  -register_mode  off -name link_in_28 "algo_top" link_in_28
+set_directive_interface -mode axis  -register_mode  off -name link_in_29 "algo_top" link_in_29
+
+set_directive_interface -mode axis  -register_mode  off -name link_in_30 "algo_top" link_in_30
+set_directive_interface -mode axis  -register_mode  off -name link_in_31 "algo_top" link_in_31
+set_directive_interface -mode axis  -register_mode  off -name link_in_32 "algo_top" link_in_32
+set_directive_interface -mode axis  -register_mode  off -name link_in_33 "algo_top" link_in_33
+set_directive_interface -mode axis  -register_mode  off -name link_in_34 "algo_top" link_in_34
+set_directive_interface -mode axis  -register_mode  off -name link_in_35 "algo_top" link_in_35
+set_directive_interface -mode axis  -register_mode  off -name link_in_36 "algo_top" link_in_36
+set_directive_interface -mode axis  -register_mode  off -name link_in_37 "algo_top" link_in_37
+set_directive_interface -mode axis  -register_mode  off -name link_in_38 "algo_top" link_in_38
+set_directive_interface -mode axis  -register_mode  off -name link_in_39 "algo_top" link_in_39
+
+set_directive_interface -mode axis  -register_mode  off -name link_in_40 "algo_top" link_in_40
+set_directive_interface -mode axis  -register_mode  off -name link_in_41 "algo_top" link_in_41
+set_directive_interface -mode axis  -register_mode  off -name link_in_42 "algo_top" link_in_42
+set_directive_interface -mode axis  -register_mode  off -name link_in_43 "algo_top" link_in_43
+set_directive_interface -mode axis  -register_mode  off -name link_in_44 "algo_top" link_in_44
+set_directive_interface -mode axis  -register_mode  off -name link_in_45 "algo_top" link_in_45
+set_directive_interface -mode axis  -register_mode  off -name link_in_46 "algo_top" link_in_46
+set_directive_interface -mode axis  -register_mode  off -name link_in_47 "algo_top" link_in_47
+
+
+set_directive_interface -mode axis  -register_mode  off -name link_out_00 "algo_top" link_out_00
+set_directive_interface -mode axis  -register_mode  off -name link_out_01 "algo_top" link_out_01
+set_directive_interface -mode axis  -register_mode  off -name link_out_02 "algo_top" link_out_02
+set_directive_interface -mode axis  -register_mode  off -name link_out_03 "algo_top" link_out_03
+set_directive_interface -mode axis  -register_mode  off -name link_out_04 "algo_top" link_out_04
+set_directive_interface -mode axis  -register_mode  off -name link_out_05 "algo_top" link_out_05
+set_directive_interface -mode axis  -register_mode  off -name link_out_06 "algo_top" link_out_06
+set_directive_interface -mode axis  -register_mode  off -name link_out_07 "algo_top" link_out_07
+set_directive_interface -mode axis  -register_mode  off -name link_out_08 "algo_top" link_out_08
+set_directive_interface -mode axis  -register_mode  off -name link_out_09 "algo_top" link_out_09
+
+set_directive_interface -mode axis  -register_mode  off -name link_out_10 "algo_top" link_out_10
+set_directive_interface -mode axis  -register_mode  off -name link_out_11 "algo_top" link_out_11
+set_directive_interface -mode axis  -register_mode  off -name link_out_12 "algo_top" link_out_12
+set_directive_interface -mode axis  -register_mode  off -name link_out_13 "algo_top" link_out_13
+set_directive_interface -mode axis  -register_mode  off -name link_out_14 "algo_top" link_out_14
+set_directive_interface -mode axis  -register_mode  off -name link_out_15 "algo_top" link_out_15
+set_directive_interface -mode axis  -register_mode  off -name link_out_16 "algo_top" link_out_16
+set_directive_interface -mode axis  -register_mode  off -name link_out_17 "algo_top" link_out_17
+set_directive_interface -mode axis  -register_mode  off -name link_out_18 "algo_top" link_out_18
+set_directive_interface -mode axis  -register_mode  off -name link_out_19 "algo_top" link_out_19
+
+set_directive_interface -mode axis  -register_mode  off -name link_out_20 "algo_top" link_out_20
+set_directive_interface -mode axis  -register_mode  off -name link_out_21 "algo_top" link_out_21
+set_directive_interface -mode axis  -register_mode  off -name link_out_22 "algo_top" link_out_22
+set_directive_interface -mode axis  -register_mode  off -name link_out_23 "algo_top" link_out_23
+set_directive_interface -mode axis  -register_mode  off -name link_out_24 "algo_top" link_out_24
+set_directive_interface -mode axis  -register_mode  off -name link_out_25 "algo_top" link_out_25
+set_directive_interface -mode axis  -register_mode  off -name link_out_26 "algo_top" link_out_26
+set_directive_interface -mode axis  -register_mode  off -name link_out_27 "algo_top" link_out_27
+set_directive_interface -mode axis  -register_mode  off -name link_out_28 "algo_top" link_out_28
+set_directive_interface -mode axis  -register_mode  off -name link_out_29 "algo_top" link_out_29
+
+set_directive_interface -mode axis  -register_mode  off -name link_out_30 "algo_top" link_out_30
+set_directive_interface -mode axis  -register_mode  off -name link_out_31 "algo_top" link_out_31
+set_directive_interface -mode axis  -register_mode  off -name link_out_32 "algo_top" link_out_32
+set_directive_interface -mode axis  -register_mode  off -name link_out_33 "algo_top" link_out_33
+set_directive_interface -mode axis  -register_mode  off -name link_out_34 "algo_top" link_out_34
+set_directive_interface -mode axis  -register_mode  off -name link_out_35 "algo_top" link_out_35
+set_directive_interface -mode axis  -register_mode  off -name link_out_36 "algo_top" link_out_36
+set_directive_interface -mode axis  -register_mode  off -name link_out_37 "algo_top" link_out_37
+set_directive_interface -mode axis  -register_mode  off -name link_out_38 "algo_top" link_out_38
+set_directive_interface -mode axis  -register_mode  off -name link_out_39 "algo_top" link_out_39
+
+set_directive_interface -mode axis  -register_mode  off -name link_out_40 "algo_top" link_out_40
+set_directive_interface -mode axis  -register_mode  off -name link_out_41 "algo_top" link_out_41
+set_directive_interface -mode axis  -register_mode  off -name link_out_42 "algo_top" link_out_42
+set_directive_interface -mode axis  -register_mode  off -name link_out_43 "algo_top" link_out_43
+set_directive_interface -mode axis  -register_mode  off -name link_out_44 "algo_top" link_out_44
+set_directive_interface -mode axis  -register_mode  off -name link_out_45 "algo_top" link_out_45
+set_directive_interface -mode axis  -register_mode  off -name link_out_46 "algo_top" link_out_46
+set_directive_interface -mode axis  -register_mode  off -name link_out_47 "algo_top" link_out_47
+
+set_directive_array_partition -type complete -dim 0 "algo_top" link_in
+set_directive_array_partition -type complete -dim 0 "algo_top" link_out
+set_directive_array_partition -type complete -dim 0 "algo_top" link_out_buf
+
