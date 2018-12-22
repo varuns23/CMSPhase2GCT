501c3e61cc30e1095ab2808169c0d0ed35ce145f
Shivangi Kamat <shivangi.kamat@gmail.com>
2018-12-22 11:56:24 -0600

    Update sources.tcl

--- sources.tcl
+++ sources.tcl
@@ -2,12 +2,12 @@
 set_top algo_top
 ##
 #### Add source code
-add_files ${TOP_DIR}/VivadoHls/null_algo/vivado_hls/src/algo_top.cpp
+add_files ${TOP_DIR}/VivadoHls/null_algo_unpacked/vivado_hls/src/algo_top.cpp
 add_files ${PROJ_DIR}/src/algo_unpacked.cpp
 add_files ${PROJ_DIR}/src/GCT.cc
 
 ### Add testbed files
-add_files -tb ${TOP_DIR}/VivadoHls/null_algo/vivado_hls/src/algo_top_tb.cpp -cflags ${CFLAGS}
+add_files -tb ${TOP_DIR}/VivadoHls/null_algo_unpacked/vivado_hls/src/algo_top_tb.cpp -cflags ${CFLAGS}
 
 ### Add data files
 add_files -tb ${PROJ_DIR}/data/zeros_algo_inp.txt
