dbf2e38ddcdf023de2bbd3623e00612510f695c7
Shivangi Kamat <shivangi.kamat@gmail.com>
2018-12-22 11:56:02 -0600

    Update algo_unpacked.cpp

--- src/algo_unpacked.cpp
+++ src/algo_unpacked.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "../../APx_Gen0_Algo/VivadoHls/null_algo/vivado_hls/src/algo_unpacked.h"   // This is where you should have had hls_algo - if not find the header file and fix this - please do not copy this file as that defines the interface
+#include "../../APx_Gen0_Algo/VivadoHls/null_algo_unpacked/vivado_hls/src/algo_unpacked.h"   // This is where you should have had hls_algo - if not find the header file and fix this - please do not copy this file as that defines the interface
 
 #include "GCT.hh"
 
